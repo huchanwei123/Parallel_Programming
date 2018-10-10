1197f5afe78aded8949591c6d2e309a8fffe543d
huchanwei123 <huchanwei1204@gmail.com>
2018-10-10 15:43:42 +0800

    update

--- homework1/advanced/advanced.c
+++ homework1/advanced/advanced.c
@@ -1,4 +1,4 @@
-
+// Description:                                             *
 //  Implementation of advanced odd-even sort which can use  *
 //  any method to sort, but elements can only be transfered *
 //  between neighbor processes.                             *
@@ -23,9 +23,9 @@
 float *MPI_Read(MPI_Comm comm, char *filename, int amode, MPI_Info info_, MPI_File fh, int offset, int count);
 void MPI_Write(MPI_Comm comm, char *filename, int amode, MPI_Info info_, MPI_File fh, float *local_data, int offset, int count);
 int comparator(const void *a, const void *b);
-void swap(float *a, float *b);
-void head_send_data(int cur_id, int local_size, float *local_data, MPI_Comm mpi_comm);
-void recv_and_sort(int cur_id, int recv_size, float *recv_data, MPI_Comm mpi_comm);
+void Merge(int size_a, int size_b, float *data_a, float *data_b, float *out);
+void Send_data(int target_id, int local_size, float *local_data, MPI_Comm mpi_comm);
+void Recv_data(int from_id, int recv_size, float *recv_data, MPI_Comm mpi_comm);
 
 int main(int argc, char *argv[]){
     // initialization for parameters
@@ -37,6 +37,7 @@ int main(int argc, char *argv[]){
     MPI_Status status;
     MPI_Comm mpi_comm = MPI_COMM_WORLD;
     MPI_Group old_group;
+
     // Initial MPI environment
     rc = MPI_Init(&argc, &argv);
     if(rc != MPI_SUCCESS){
@@ -50,21 +51,21 @@ int main(int argc, char *argv[]){
     // if the number of process is larger than N...
     if(N < size){
     	// obtain the group of processes in the world comm.
-	MPI_Comm_group(mpi_comm, &old_group);
+        MPI_Comm_group(mpi_comm, &old_group);
 
-	// Remove unnecessary processes
-	MPI_Group new_group;
-	int ranges[][3] = {{N, size-1, 1}};
-	MPI_Group_range_excl(old_group, 1, ranges, &new_group);
+        // Remove unnecessary processes
+        MPI_Group new_group;
+        int ranges[][3] = {{N, size-1, 1}};
+        MPI_Group_range_excl(old_group, 1, ranges, &new_group);
 
-	// Create new comm.
-	MPI_Comm_create(mpi_comm, new_group, &mpi_comm);
+        // Create new comm.
+        MPI_Comm_create(mpi_comm, new_group, &mpi_comm);
 
-	if(mpi_comm == MPI_COMM_NULL){
-	    MPI_Finalize();
-	    exit(0);
-	}
-	size = N;
+        if(mpi_comm == MPI_COMM_NULL){
+            MPI_Finalize();
+            exit(0);
+        }
+        size = N;
     }
 
     // scatter the data to each processor
@@ -82,57 +83,78 @@ int main(int argc, char *argv[]){
     local_data = MPI_Read(mpi_comm, argv[2], MPI_MODE_RDONLY, MPI_INFO_NULL, file_in, offset, local_size);
     
     // Start processor level odd-even sort
-    int done = 0;
-    int even_done = 0;
-    int odd_done = 0;
     int phase = 0;
+
+    // perform local sort first
+    qsort(local_data, local_size, sizeof(float), comparator);
+
+    // First, partner processor will communicate with each other of their size
+    int left_size = 0;
+    int right_size = 0;
     
-    // define pointer of pointer to local data :)
-    float *p2local_data;
-    float *get_data = malloc(local_size * sizeof(float));
-    p2local_data = &(local_data[0]);
-   
-    if(id == 2){
-        head_send_data(id, local_size, local_data, mpi_comm);
-    }else if(id == 1){
-        printf("[1] origin addr. of get_data: %p\n", get_data);
-        recv_and_sort(id, local_size, get_data, mpi_comm);
-        printf("[1] Addr. of get_data: %p\n", get_data);
-        printf("[1] Get data from 2 with: %f\n", *(get_data+1));
-    }else;
-    /*
-    while(!even_done || !odd_done){
-        done = 1;
-        qsort(local_data, local_size, sizeof(float), comparator);
-
-        if(is_odd(id) && id != size-1){
-            // in odd phase now
-            done = tail_send2head(id, local_size, local_data, done, mpi_comm);
-            done = head_recv_from_tail(id, local_data, done, mpi_comm);
-        }else if(is_even(id) && id != size-1){
-            // in even phase now
-            done = tail_send2head(id, local_size, local_data, done, mpi_comm);
-            done = head_recv_from_tail(id, local_data, done, mpi_comm);
-        }
-        else;
-        // change phase
-        phase = ((phase == ODD_PHASE)? EVEN_PHASE : ODD_PHASE);
-
-        // wait all processes done
-        MPI_Barrier(mpi_comm);
-        if(phase == ODD_PHASE)
-            MPI_Allreduce(&done, &even_done, 1, MPI_INT, MPI_LAND, mpi_comm);
-        else
-            MPI_Allreduce(&done, &odd_done, 1, MPI_INT, MPI_LAND, mpi_comm);
+    if(id != 0){
+        MPI_Send(&local_size, 1, MPI_INT, id-1, 2, mpi_comm);
+        MPI_Recv(&left_size, 1, MPI_INT, id-1, 2, mpi_comm, MPI_STATUS_IGNORE);
     }
 
+    if(id != size-1){
+        MPI_Recv(&right_size, 1, MPI_INT, id+1, 2, mpi_comm, MPI_STATUS_IGNORE);
+        MPI_Send(&local_size, 1, MPI_INT, id+1, 2, mpi_comm);
+    }
+
+    
+    float *my_temp = malloc(right_size * sizeof(float));
+    float *out = malloc((right_size+local_size) * sizeof(float));
+    // start sorting
+    int i = 0;
+    for(phase=0; phase<size; phase++){
+        if(is_even(phase)){
+            // in even phase
+            if(is_even(id) && id != size-1){
+                // I'm even id in even phase, so I have a id+1 partner
+                Recv_data(id+1, right_size, my_temp, mpi_comm);
+                Merge(local_size, right_size, local_data, my_temp, out);
+                for(i=0; i<local_size; i++) local_data[i] = out[i];
+                Send_data(id+1, right_size, &out[local_size], mpi_comm);
+            }else{
+                // I'm odd id in even phase
+                Send_data(id-1, local_size, local_data, mpi_comm);
+                Recv_data(id-1, local_size, local_data, mpi_comm);
+            }            
+        }
+        else
+            // in odd phase
+            if(is_odd(id) && id != size-1){
+                // I'm odd id in odd phase, so I have a id+1 partner
+                Recv_data(id+1, right_size, my_temp, mpi_comm);
+                Merge(local_size, right_size, local_data, my_temp, out);
+                for(i=0; i<local_size; i++) local_data[i] = out[i];
+                Send_data(id+1, right_size, &out[local_size], mpi_comm);
+            }else if(is_even(id) && id != 0){
+                // I'm even id in even phase
+                Send_data(id-1, local_size, local_data, mpi_comm);
+                Recv_data(id-1, local_size, local_data, mpi_comm);
+            }else;
+        MPI_Barrier(mpi_comm);
+    }
+    
+    // print sorted result
+    printf("I'm ID [%d]\n", id);
+    for(i = 0; i<local_size; i++){
+        printf("%f\n", local_data[i]);
+    }
+
+
+    
     // write the result
     MPI_Write(mpi_comm, argv[3], MPI_MODE_CREATE|MPI_MODE_WRONLY, MPI_INFO_NULL, file_out, local_data, offset, local_size);
 
     // free the data
     free(local_data);
-    */
-    MPI_Barrier(mpi_comm);   
+    free(my_temp);
+    free(out);
+    
+    //MPI_Barrier(mpi_comm);   
     MPI_Finalize();
 
     return 0;
@@ -169,25 +191,42 @@ float *MPI_Read(MPI_Comm comm, char *filename, int amode, MPI_Info info_, MPI_Fi
     return local_data;
 }
 
-void head_send_data(int cur_id, int local_size, float *local_data, MPI_Comm mpi_comm){
-    // send the address to cur_id-1
-    MPI_Send(local_data, local_size, MPI_FLOAT, cur_id-1, 1, mpi_comm);
+void Send_data(int target_id, int local_size, float *local_data, MPI_Comm mpi_comm){
+    // send data to target_id
+    MPI_Send(local_data, local_size, MPI_FLOAT, target_id, 1, mpi_comm);
+    return;
 }
 
-void recv_and_sort(int cur_id, int recv_size, float *recv_data, MPI_Comm mpi_comm){
-    // recveive the address from cur_id+1
-    MPI_Recv(recv_data, recv_size, MPI_FLOAT, cur_id+1, 1, mpi_comm, MPI_STATUS_IGNORE);
+void Recv_data(int from_id, int recv_size, float *recv_data, MPI_Comm mpi_comm){
+    // recveive data from from_id
+    MPI_Recv(recv_data, recv_size, MPI_FLOAT, from_id, 1, mpi_comm, MPI_STATUS_IGNORE);
     return;
 }
 
 int comparator(const void *a, const void *b){
+    // need to change to the other
     return (*(float *)a - *(float *)b);
 }
 
-void swap(float *a, float *b){
-    float tmp;
-    tmp = *b;
-    *b = *a;
-    *a = tmp;
-    return;
+void Merge(int size_a, int size_b, float *data_a, float *data_b, float *out){
+    // Merge the list with lower order to out
+    int idx_a = 0, idx_b = 0, idx_out = 0;
+    int total_size = size_a+size_b;
+    while(idx_out < total_size && idx_a < size_a && idx_b < size_b){
+        if(data_a[idx_a] < data_b[idx_b])
+            out[idx_out++] = data_a[idx_a++];
+        else
+            out[idx_out++] = data_b[idx_b++];
+    }
+    if(idx_out != total_size-1){
+        if(idx_a == size_a-1){
+            while(idx_out<total_size){
+                out[idx_out++] = data_b[idx_b++];
+            } 
+        }else{
+            while(idx_out<total_size){
+                out[idx_out++] = data_a[idx_a++];
+            } 
+        }
+    }
 }
